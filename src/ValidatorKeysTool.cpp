ea2a5ea68c6ee0afbf1607ab351e734721553ae7
wilsonianb <bwilson@ripple.com>
2018-02-02 15:21:42 -0600

    Set version to 0.2.1

--- src/ValidatorKeysTool.cpp
+++ src/ValidatorKeysTool.cpp
@@ -44,7 +44,7 @@ char const* const versionString =
     //  The build version number. You must edit this for each release
     //  and follow the format described at http://semver.org/
     //
-        "0.2.0"
+        "0.2.1"
 
 #if defined(DEBUG) || defined(SANITIZER)
        "+"
